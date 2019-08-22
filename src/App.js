e1ca7fa3ff7787c8d77d44a1d369dd4168124ed0
roselandroche <r.landroche@gmail.com>
2019-08-21 21:30:21 -0400

    Stretch goalssss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,6 +7,25 @@ function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, updateHomeScore] = useState(0)
   const [awayScore, updateAwayScore] = useState(0)
+  const [timer, updateTimer] = useState(15*60)
+  const [quarter, updateQuarter] = useState(1)
+  const [timerRunning, updateTimerRunning] = useState(false)
+
+  useEffect(() => {
+    if(timerRunning) {
+      const interval = setInterval(() => {
+          updateTimer(timer => timer-1)
+        }, 1000);
+      return () => clearInterval(interval)
+    }
+  }, [timerRunning])
+
+  useEffect(() => {
+    if(timer<1) {
+      updateTimer(15*60)
+      updateQuarter(quarter+1)
+    } 
+  }, [timer, quarter])
 
   return (
     <div className="container">
@@ -19,13 +38,15 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer" onClick={() => updateTimerRunning(!timerRunning)}>
+            {`${(Math.floor(timer/60)).toString().padStart(2,'0')}:${(timer%60).toString().padStart(2,'0')}`}
+          </div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow timer={timer} quarter={quarter}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const BottomRow = () => {
+const BottomRow = (props) => {
+  console.log(props)
+  const [quarter, updateQuarter] = useState(0)
+  
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +21,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value" >{props.quarter}</div>
       </div>
     </div>
   );
